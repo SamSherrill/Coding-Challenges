3af18ff4f5e704593b7520143af8848c2ad757f0
KaiChunChan <jackson.chan@familysearch.org>
2024-05-07 14:57:10 -0600

    Jackson's idea

---
+++ Find Prime Number/main.js
@@ -0,0 +1,12 @@
+
+function isPrime(number) {
+//Complete the function to return true when the number is a prime number, 
+//false when the number is not a prime number
+
+
+
+}
+
+// Example usage
+console.log(isPrime(7)); // Output: true
+console.log(isPrime(12)); // Output: false
