d20d01347a772ebf0166582e4e24fbe87e21736b
Sam Sherrill <38333695+SamSherrill@users.noreply.github.com>
2024-05-16 15:10:45 -0400

    Fib main seems to work with hard coded inputs; needs refactoring to simplify, then needs user input from terminal enabled.

--- Fibonacci/main.js
+++ Fibonacci/main.js
@@ -1,16 +1,27 @@
-const previousValue = 0;
-const currentValue = 1;
-const placeInSeq = 10;
+const fibVal1 = 1;
+const fibVal2 = 1;
+const userIntInput = 5;
 
-// Input for user is placeInSeq
-// Output Fibonacci val at that place
+// Input from user will become userIntInput; it must be a whole # INT
+// Output Fibonacci val at that place in the Fib sequence
 
-function fibonacciNow (previousValue, currentValue, placeInSeq) {
-    for (let i = 0; i <= placeInSeq; i++) {
-        currentValue = previousValue + currentValue;
-        console.log(currentValue);
+function fibonacciNow(previousValue, currentValue, placeInSeq) {
+  if (placeInSeq == 1 || placeInSeq == 2) {
+    console.log(1);
+  } else {
+    console.log(1);
+    console.log(1);
+    for (let i = 3; i <= placeInSeq; i++) {
+      a = previousValue;
+      b = currentValue;
+      currentValue = a + b;
+      previousValue = b;
+      console.log(currentValue);
     }
+  }
 }
 
+console.log("I'm alive.");
 
-// 1, 1, 2, 3, 
+fibonacciNow(fibVal1, fibVal2, userIntInput);
+// 1, 1, 2, 3, 5, 8, 13, 21, 34
